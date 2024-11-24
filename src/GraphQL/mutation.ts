3a3bab91ed8df1d050bc69a41f5123c04e172a73
IceeeAge <izmealsjh@gmail.com>
2024-11-24 17:04:40 +0800

    New Update mutation create patient

--- src/GraphQL/mutation.ts
+++ src/GraphQL/mutation.ts
@@ -28,11 +28,78 @@ export const CREATE_PATIENT_MUTATION = gql`
         occupation: $occupation
         weight: $weight
         statusAppointment: $statusAppointment
+        bleedingGums: "N/A"
+        sensitive: "N/A"
+        foodTrap: "N/A"
+        dryMouth: "N/A"
+        pastGap: "N/A"
+        pastOrthodonic: "N/A"
+        problem: "N/A"
+        earaches: "N/A"
+        clicking: "N/A"
+        bruxing: "N/A"
+        sores: "N/A"
+        dentures: "N/A"
+        active: "N/A"
+        serious: "N/A"
+        dateOfLastExam: "N/A"
+        careOfAPhysicians: "N/A"
+        physiciansName: "N/A"
+        phonePhysician: "N/A"
+        areYouInGoodHealth: "N/A"
+        anyChangesInYourHealth: "N/A"
+        hospitalization: "N/A"
+        prescriptionOrCounterMedications: "N/A"
+        allergies: "N/A"
+        specify: "N/A"
+        alcoholIntake: "N/A"
+        amoutAndFrequency: "N/A"
+        forWomen: "N/A"
+        lactating: "N/A"
+        ifYesWhatWasTheIllnessOrProblem: "N/A"
+        dateOfLastPhysicalExam: "N/A"
+        vitaminsNaturalOrHerbalSupplements: "N/A"
+        highBloodPressure: "NO"
+        heartAttack: "NO" 
+        heartDiseaseSpecify: "N/A"
+        lowBloodPressure: "NO"
+        leukemia: "NO"
+        anemia: "NO"
+        kidneyDisease: "NO"
+        kidneyDiseaseSpecify: "N/A"
+        asthma: "NO"
+        lungDisease: "NO"
+        lungDiseaseSpecify: "N/A"
+        stroke: "NO"
+        neurologicalDisorder: "NO"
+        neurologicalDisorderSpecify: "N/A"
+        epilepsyConvulsions: "NO"
+        brainInjury: "NO"
+        psychiatricTreatment: "NO"
+        diabetes: "NO"
+        aidsHivInfection: "NO"
+        congenitalDisease: "NO"
+        congenitalDiseaseSpecify: "N/A"
+        hepatitis: "NO"
+        liverDisorder: "NO"
+        livErDisorderSpecify: "N/A"
+        jointDisorder: "NO"
+        jointReplacementOrImplants: "NO"
+        steroidTherapy: "NO"
+        stomachTroublesUlcers: "NO"
+        chestPains: "NO"
+        frequentSoreThroat: "NO"
+        persistentCough: "NO"
+        difficultInBreathing: "NO"
+        cancerOrTumor: "NO"
+        radiationOrChemotherapy: "NO"
+        recentWEightLoss: "NO"
+        others: "NO"
+        othersSpecify: "N/A"
       }
     ) {
       id
     }
-
     publishManyPatients(to: PUBLISHED) {
       count
     }
@@ -195,18 +262,26 @@ export const UPDATE_PATIENT_QUESTIONNAIRE = gql`
 `;
 
 export const REQUEST_EFORM = gql`
- mutation RequestUserEformPdfFile ($email: String!, $fullName: String!, $requestUserEformPdf: String!) {
-  createRequestUserEformPdf(
-    data: {email: $email, fullName: $fullName, requestUserEformPdf: $requestUserEformPdf}
+  mutation RequestUserEformPdfFile(
+    $email: String!
+    $fullName: String!
+    $requestUserEformPdf: String!
   ) {
-    id
-  }
-  publishManyRequestUserEformPdfsConnection {
-    edges {
-      node {
-        id
+    createRequestUserEformPdf(
+      data: {
+        email: $email
+        fullName: $fullName
+        requestUserEformPdf: $requestUserEformPdf
+      }
+    ) {
+      id
+    }
+    publishManyRequestUserEformPdfsConnection {
+      edges {
+        node {
+          id
+        }
       }
     }
   }
-}
 `;
